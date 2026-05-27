19b9180e5528c550ebd8cd8680ef525f80c8723f
nicolasdev97 <nicolas.po.im@gmail.com>
2026-05-27 17:56:55 -0500

    Exercise 14.22.

--- part14/blogapp-nextjs/app/page.tsx
+++ part14/blogapp-nextjs/app/page.tsx
@@ -1,7 +1,15 @@
-export default function HomePage() {
+import Homepage from "./homepage.mdx";
+
+export default function Home() {
   return (
-    <div>
-      <h1>Welcome to the Blog App on Vercel</h1>
-    </div>
+    <main className="min-h-screen bg-zinc-950 text-zinc-100">
+      <div className="max-w-5xl mx-auto px-6 py-6">
+        <div className="bg-zinc-900 border border-zinc-800 rounded-3xl shadow-2xl p-10">
+          <div className="markdown">
+            <Homepage />
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
